3f239414ddabb32b03b87532826a43c61a42678d
uonr <dev@yuru.me>
2024-08-07 13:00:07 +0900

    feat: rich-textarea

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import "./App.css";
+import { RichTextarea } from "rich-textarea";
 
 function App() {
   const [input, setInput] = useState("");
+  const renderer = (value: string): ReactNode[] => {
+    return [<span>{value}</span>];
+  };
   return (
     <>
-      <textarea
+      <RichTextarea
         className="editor"
         value={input}
+        style={{
+          width: "100%",
+          height: "100%",
+          resize: "none",
+          border: "none",
+          fontFamily: "monospace",
+          fontSize: "15px",
+        }}
         onChange={(e) => setInput(e.target.value)}
-      />
+      >
+        {renderer}
+      </RichTextarea>
     </>
   );
 }
